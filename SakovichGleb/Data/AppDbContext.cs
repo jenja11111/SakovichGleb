549732a2bd8a4b860ab192250ab14d4d2991463b
mea <mea@108-44>
2022-11-15 19:33:23 +0300

    Добавьте файлы проекта.

---
+++ SakovichGleb/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IndividualPlan.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+
+        }
+    }
+}
